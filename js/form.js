978139c1395a47c46bf646c26af64775e2dfa715
karinaramos0401 <84257110+karinaramos0401@users.noreply.github.com>
2022-01-17 09:07:27 -0300

    update

--- js/form.js
+++ js/form.js
@@ -1,23 +1,37 @@
+//pegar todos os formulários
 const forms = document.querySelectorAll('[data-form]')
+//pegar todos os botões
 const buttons = document.querySelectorAll('[data-button]');
-buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    const formValue = button.dataset.button
-    button.classList.add('button--active')
+buttons.forEach((clickedButton) => {
+  //quando eu clicar em um botão
+  clickedButton.addEventListener('click', () => {
+    const formValue = clickedButton.dataset.button
+    buttons.forEach((button) => {
+      //removendo/inativando os botões
+      button.classList.remove('button--active')
+    })
+    //ativando somente o botão clicado
+    clickedButton.classList.add('button--active')
     forms.forEach((form) => {
+      // qual é o formulário relacionado a esse botão? (botão clicado)
       if(form.dataset.form === formValue) {
+        //quero que mostre o formulário relacionado
         form.classList.add('form--active')
       } else {
+        //e esconda os outros 
         form.classList.remove('form--active')
       }
+      buttons.forEach()
     })
   })
 })
 
-//pegar todos os botões
-//pegar todos os formulários
-//quando eu clicar em um botão
-// qual é o formulário relacionado a esse botão? (botão clicado)
-//quero que mostre o formulário relacionado
-//e esconda os outros 
 
+
+
+
+
+
+
+//dataset é uma propriedade de leitura que vai dar acesso aos dados do atributo data colocado nos elementos expondo um mapa de strings, mostrando a entrada de cada data. 
+//Sendo lido como // element.dataset.keyname ou .example
